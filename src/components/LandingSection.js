59ee9ef543d7eca375c2ff0ad5c9a3968bef5d38
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-03-23 12:01:27 -0300

    [EDITED] heading section

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -14,7 +14,7 @@ const LandingSection = () => (
   >
     <VStack spacing={4}>
       <Avatar name='Joaquin Martinez Maneiro' size='2xl' src='https://media.licdn.com/dms/image/C4D03AQG24bbuq74ngQ/profile-displayphoto-shrink_800_800/0/1661521752175?e=1681948800&v=beta&t=VZpMMGMr8m1r4yRo28llbqwbniZ4ea2fjLP-5Kerm3A' />
-      <Heading size="md">{greeting}</Heading>
+      <Heading size="lg">{greeting}</Heading>
       <Heading mt="40px !important">{bio1}</Heading>
     </VStack>
   </FullScreenSection>
